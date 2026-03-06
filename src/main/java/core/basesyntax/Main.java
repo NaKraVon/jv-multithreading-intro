5d1621f877a223abdee8f8f29abcd51b6f420367
mr-Edoc <mr.edoc.work@gmail.com>
2026-03-06 17:51:42 +0200

    Added threads, one expend Thread other implement Runnable. The one what expend Thread is daemon thread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,28 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        Thread t1 = new Thread(myRunnable);
+        t1.start();
+
+        MyThread daemonThread = new MyThread();
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+    }
+
+    static class MyThread extends Thread {
+        public void run() {
+            while (true) {
+                System.out.println("I am the daemon thread!");
+            }
+        }
+    }
+
+    static class MyRunnable implements Runnable {
+        public void run() {
+            for (int i = 0; i <= 20; i++) {
+                System.out.println(i);
+            }
+        }
     }
 }
